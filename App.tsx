a8870275e626864be8e307db5a3c6a885fbbb19f
JoseGustavoCabralMello <gu.mello.gm@gmail.com>
2025-03-22 19:24:44 -0300

    CSS-in-JS - Definindo o tema da aplicação

--- App.tsx
+++ App.tsx
@@ -1,7 +1,12 @@
+import { ThemeProvider } from 'styled-components/native';
 import { Groups } from './src/screens/Groups';
+import theme from '@theme/index';
 
 export default function App() {
   return (
+    <ThemeProvider theme={theme}>
+      
    <Groups />
+    </ThemeProvider>
   )
 }
