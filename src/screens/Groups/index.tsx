7b23fe13310a956aa8e5adeb83cad9ece5562a3d
JoseGustavoCabralMello <gu.mello.gm@gmail.com>
2025-03-23 18:35:31 -0300

    CSS-in-JS - Componente List Empty

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -4,9 +4,10 @@ import { Highlight } from '@components/Highlight';
 import { GroupCard } from '@components/GroupCard';
 import { useState } from 'react';
 import { FlatList } from 'react-native';
+import { ListEmpty } from '@components/ListEmpty';
 
 export function Groups() {
-  const [grous, setGroups] = useState<string[]>(['Galera da Rocket']);
+  const [groups, setGroups] = useState<string[]>([]);
 
   return (
     <Container>
@@ -18,13 +19,17 @@ export function Groups() {
        />
 
       <FlatList
-        data={grous}
+        data={groups}
         keyExtractor={item => item}
         renderItem={({ item }) => (
           <GroupCard
             title={item}
           />
         )}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+         ListEmptyComponent={() => (
+           <ListEmpty message="Que tal cadastrar a primeira turma?" />
+         )}
       />
     </Container>
   );
