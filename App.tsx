e8f55b77997a3d5dca39a0fc356465cfe0cfa79c
JoseGustavoCabralMello <gu.mello.gm@gmail.com>
2025-03-23 16:29:02 -0300

    CSS-in-JS - Estilizando a StatusBar

--- App.tsx
+++ App.tsx
@@ -5,12 +5,18 @@ import theme from '@theme/index';
 
 import { Groups } from './src/screens/Groups';
 import { Loading } from 'src/componets/Loading';
+import { StatusBar } from 'react-native';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
       {fontsLoaded ? <Groups /> : <Loading/> }
     </ThemeProvider>
   )
