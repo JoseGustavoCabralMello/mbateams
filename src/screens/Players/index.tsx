86afb3ec8b75df9f2df5b69157d90933e2af7f3f
JoseGustavoCabralMello <gu.mello.gm@gmail.com>
2025-03-24 20:47:21 -0300

    CSS-in-JS - Deixando o ícone dinâmico

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,20 +1,30 @@
 import { Header } from "@components/Header";
- import { Highlight } from "@components/Highlight";
- 
- import { Container } from "./styles";
-import { ButtonIcon } from "@components/ButtonIcon";
- 
- export function Players() {
-   return (
-     <Container>
-       <Header showBackButton />
- 
-       <Highlight 
-         title="Nome da turma"
-         subtitle="adicione a galera e separe os times"
-       />
+import { Highlight } from "@components/Highlight";
 
-       <ButtonIcon />
-     </Container>
-   )
- }
+import { Container, Form } from "./styles";
+import { ButtonIcon } from "@components/ButtonIcon";
+import { Input } from "@components/Input";
+ 
+export function Players() {
+  return (
+    <Container>
+      <Header showBackButton />
+
+      <Highlight 
+        title="Nome da turma"
+        subtitle="adicione a galera e separe os times"
+      />
+
+      <Form>
+        <Input 
+          placeholder="Nome da pessoa"
+          autoCorrect={false}
+        />
+
+        <ButtonIcon 
+          icon="add" 
+        />
+      </Form>
+    </Container>
+  )
+}
