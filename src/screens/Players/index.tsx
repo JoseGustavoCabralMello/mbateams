955bf3ee9cf671c3483f17144f426b4f93608c40
JoseGustavoCabralMello <gu.mello.gm@gmail.com>
2025-03-24 21:55:31 -0300

    CSS-in-JS - Finalizando lista de Players

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -8,6 +8,8 @@ import { Filter } from "@components/Filter";
 import { FlatList } from "react-native";
 import { useState } from "react";
 import { PlayerCard } from "@components/PlayerCard";
+import { ListEmpty } from "@components/ListEmpty";
+import { Button } from "@components/Button";
  
 export function Players() {
   const [team, setTeam] = useState('Time A')
@@ -61,7 +63,17 @@ export function Players() {
              onRemove={() => {}}
            />
          )}
-       />
+          ListEmptyComponent={() => (
+            <ListEmpty message="Não há pessoas nesse time" />
+            )}
+            showsVerticalScrollIndicator={false}
+            contentContainerStyle={[{ paddingBottom: 100 }, players.length === 0 && { flex: 1 }]}
+          />
+
+          <Button 
+            title="Remover Turma"
+            type="SECONDARY"
+        />
     </Container>
   )
 }
